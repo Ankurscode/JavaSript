4b2967df2b70e47f990405c57797de2a808f0520
Ankur yadav <165983779+Ankurscode@users.noreply.github.com>
2024-10-06 17:03:06 +0530

    First one

---
+++ First_1/script.js
@@ -0,0 +1,11 @@
+function  convetToforenheit(celsius){
+    return (celsius*9/5)+35;
+}
+console.log(`Forenheit:${convetToforenheit(88)}`);
+
+
+
+function convertTocelsius(forenheit){
+    return (forenheit-32)*5/9;
+}
+console.log(`Celsius:${convertTocelsius(98)}`);
